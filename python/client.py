30c9f356b3b2e5076679209321abb981f39eef53
hordur palsson <hordur.palsson10@gmail.com>
2025-10-02 10:46:08 +0000

    update

--- python/client.py
+++ python/client.py
@@ -1,12 +1,13 @@
 import socket
+
 # Variables
-ip = "172.0.0.1"
+ip = "127.0.0.1"
 port = 4242
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.settimeout(1)
 
-client_socket.sendto("Hello from python!".encode(), (ip, port))
+client_socket.sendto("Hello from Python!".encode(), (ip, port))
 
 data, (recv_ip, recv_port) = client_socket.recvfrom(1024)
-print(f"Recieved: '{data.decode()}' {recv_ip}:{recv_port}")
+print(f"Received: '{data.decode()}' {recv_ip}:{recv_port}")
